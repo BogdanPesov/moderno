a03af6a4bd7b3872300456e7661949801133a39f
BogdanPesov <bodyapesov@gmail.com>
2020-03-03 20:03:37 +0400

    added product slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function() { /* запись функции такова, чтобы сразу код читался из файла html до конца, а потом уже смотрел в эту функцию */
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
 
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+    var mixer = mixitup('.products__inner-box');
 
 
 
